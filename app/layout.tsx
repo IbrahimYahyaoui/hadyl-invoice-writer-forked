d2389aff0c0672c768aa2196aa9f5247440fa0b3
Ibrahim Yahyaoui <ibrahimbnhs@gmail.com>
2024-07-15 16:55:29 +0100

    add next analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Invoice",
@@ -18,7 +19,10 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=1024" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
